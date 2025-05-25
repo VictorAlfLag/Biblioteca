6d618d214e3e11bf073570e602a88a400ef186ae
VictorAlfLag <victor.chiguano7102@utc.edu.ec>
2025-05-25 03:17:16 -0500

    Preparando limpieza para despliegue de estáticos

--- Biblioteca/settings.py
+++ Biblioteca/settings.py
@@ -136,18 +136,17 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Biblioteca/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Biblioteca/static')] # ¡Esta es la ruta correcta según tu confirmación!
 
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# ...
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
