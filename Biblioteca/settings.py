70b5ba67bacc77d1a0699a715f4332462408b9c1
VictorAlfLag <victor.chiguano7102@utc.edu.ec>
2025-05-26 00:32:04 -0500

    Fix WhiteNoiseMiddleware import and static/media serving

--- Biblioteca/settings.py
+++ Biblioteca/settings.py
@@ -152,13 +152,16 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # Configuración de archivos estáticos
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Esta línea es importante
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Biblioteca/static'),)
 
-# Opcional: Para permitir que Whitenoise sirva archivos estáticos comprimidos y con caché
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#Validations
+if not DEBUG:
+    STATICFILES_ROOT =os.path.join(BASE_DIR,'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ...
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Biblioteca/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
